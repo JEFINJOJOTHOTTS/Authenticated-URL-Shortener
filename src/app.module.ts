10d5a4d36a58d7e1a7bf51d5ab59240920237a10
JEFINJOJOTHOTTS <jefinjojo@gmail.com>
2024-01-15 17:15:18 +0530

    routing

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,13 @@ import { AppService } from './app.service';
 import { ComponentsModule } from './components/user/users.module';
 import { UrlModule } from './components/url/url.module';
 import { UrlService } from './dbsrvs/srv/url/url.service';
-
+import {RouterModule} from '@nestjs/core'
 @Module({
-  imports: [ComponentsModule, UrlModule],
+  imports: [ComponentsModule, UrlModule, RouterModule.register([{
+    path: 'minify-url',
+    module: UrlModule,
+  },])],
   controllers: [AppController],
   providers: [AppService, UrlService],
 })
-export class AppModule {}
+export class AppModule { }
